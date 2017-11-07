836128e6f2022a5fac8943e671ff4062dcbc5d89
HugoLuna <hugo_1199@hotmail.com>
2017-11-07 00:16:47 -0600

    Initial commit

---
+++ src/Principal/Main.java
@@ -0,0 +1,27 @@
+package Principal;
+
+import Interfaz.Juego;
+
+/**
+ *
+ * @author HugoLuna
+ * @email  hugo@lunainc.org
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+            new Juego().setVisible(true);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Juego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    }
+
+}
